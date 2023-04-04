e99cc5c1a003e40e7315d7c810262c10a8af9197
BanuL01 <banuji01liyanwala@gmail.com>
2023-04-04 15:20:18 +0530

    invite page

---
+++ CricOzy/lib/model/invite_friend_screen.dart
@@ -0,0 +1,125 @@
+import 'package:best_flutter_ui_templates/model/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class InviteFriend extends StatefulWidget {
+  @override
+  _InviteFriendState createState() => _InviteFriendState();
+}
+
+class _InviteFriendState extends State<InviteFriend> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var brightness = MediaQuery.of(context).platformBrightness;
+    bool isLightMode = brightness == Brightness.light;
+    return Container(
+      color: isLightMode
+          ? Color.fromARGB(255, 248, 236, 228)
+          : AppTheme.nearlyBlack,
+      child: SafeArea(
+        top: false,
+        child: Scaffold(
+          backgroundColor: isLightMode
+              ? Color.fromARGB(255, 248, 236, 228)
+              : AppTheme.nearlyBlack,
+          body: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).padding.top,
+                    left: 16,
+                    right: 16),
+                child: Image.asset('assets/images/inviteImage.png'),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 8),
+                child: Text(
+                  'Invite Your Friends',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: isLightMode ? Colors.black : Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  'Are you one of those who makes everything\n at the last moment?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: isLightMode ? Colors.black : Colors.white,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Container(
+                      width: 120,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: isLightMode
+                            ? Color.fromARGB(255, 19, 33, 55)
+                            : Colors.white,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(4.0)),
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(0.6),
+                              offset: const Offset(4, 4),
+                              blurRadius: 8.0),
+                        ],
+                      ),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {
+                            //method here for functionality
+                            print('Share Action.');
+                          },
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.share,
+                                  color:
+                                      isLightMode ? Colors.white : Colors.black,
+                                  size: 22,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Text(
+                                    'Share',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      color: isLightMode
+                                          ? Colors.white
+                                          : Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

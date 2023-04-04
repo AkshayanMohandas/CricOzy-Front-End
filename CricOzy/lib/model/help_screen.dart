0993ef1934d0630a34c2eb274d1703d83e49c3f6
Daniel0Fernando <daniel.20200947@iit.ac.lk>
2023-04-04 14:45:01 +0530

    help screen

---
+++ CricOzy/lib/model/help_screen.dart
@@ -0,0 +1,107 @@
+import 'package:best_flutter_ui_templates/model/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class HelpScreen extends StatefulWidget {
+  @override
+  _HelpScreenState createState() => _HelpScreenState();
+}
+
+class _HelpScreenState extends State<HelpScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var brightness = MediaQuery.of(context).platformBrightness;
+    bool isLightMode = brightness == Brightness.light;
+    return Container(
+      color: isLightMode
+          ? Color.fromARGB(255, 248, 236, 228)
+          : AppTheme.nearlyBlack,
+      child: SafeArea(
+        top: false,
+        child: Scaffold(
+          backgroundColor: isLightMode
+              ? Color.fromARGB(255, 248, 236, 228)
+              : AppTheme.nearlyBlack,
+          body: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).padding.top,
+                    left: 16,
+                    right: 16),
+                child: Image.asset('assets/images/helpImage.png'),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 8),
+                child: Text(
+                  'How can we help you?',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: isLightMode ? Colors.black : Colors.white),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  'It looks like you are experiencing problems\nwith our sign up process. We are here to\nhelp so please get in touch with us',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: isLightMode ? Colors.black : Colors.white),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Container(
+                      width: 140,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: isLightMode
+                            ? Color.fromARGB(255, 19, 33, 55)
+                            : Colors.white,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(4.0)),
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(0.6),
+                              offset: const Offset(4, 4),
+                              blurRadius: 8.0),
+                        ],
+                      ),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {},
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Text(
+                                'Chat with Us',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  color:
+                                      isLightMode ? Colors.white : Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

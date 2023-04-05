c98fbde24cb9280475090a1329443eeaf671389e
BanuL01 <banuji01liyanwala@gmail.com>
2023-04-05 18:29:45 +0530

    weather-page-update

--- CricOzy/lib/weather/weather_api_client.dart
+++ CricOzy/lib/weather/weather_api_client.dart
@@ -2,8 +2,11 @@ import 'dart:convert';
 
 import 'package:best_flutter_ui_templates/weather/weather_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/src/mock_client.dart';
 
 class WeatherApiClient {
+  set httpClient(MockClient httpClient) {}
+
   Future<WeatherModel> getCurrentWeather(String? location) async {
     var endPoint = Uri.parse(
         "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=c06ccef3f85fba1f9f98b66795ae4a7b&units=metric");
